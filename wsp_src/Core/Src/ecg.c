29a610414729a358577842db29dd6f67e49f7187
Michael Nolan <mtnolan2640@gmail.com>
2021-03-25 13:42:11 -0400

    Add FIR filter coefficients and machinery

--- wsp_src/Core/Src/ecg.c
+++ wsp_src/Core/Src/ecg.c
@@ -12,18 +12,36 @@
 #include "main.h"
 #include "arm_math.h"
 
-#define ADC_SAMPLE_COUNT 4
+#define ADC_SAMPLE_COUNT 32
+typedef float32_t sample_type_t;
 
 uint8_t ecg_enabled;
 
 // Double buffer to allow the ADC samples to be captured while the main code is working on filtering the other buffer
-uint16_t sample_buffer1[ADC_SAMPLE_COUNT];
-uint16_t sample_buffer2[ADC_SAMPLE_COUNT];
-uint16_t *sample_ptr, *sample_end;
+sample_type_t sample_buffer1[ADC_SAMPLE_COUNT];
+sample_type_t sample_buffer2[ADC_SAMPLE_COUNT];
+sample_type_t *sample_ptr, *sample_end;
 
 // Flags to tell the mainline code when a buffer has been filled and which buffer it is
 volatile uint8_t buffer_finished;
-volatile uint16_t *buffer_ptr;
+volatile sample_type_t *buffer_ptr;
+
+//FIR Filter state and coefficients
+#define BLOCK_SIZE ADC_SAMPLE_COUNT
+#define NUM_TAPS 11
+
+// Input will be taken from one of the sample buffers above
+// Filter output will be placed here
+static sample_type_t filter_output[BLOCK_SIZE];
+// Scratch space for the filter function
+static sample_type_t fir_state[BLOCK_SIZE + NUM_TAPS - 1];
+// filter coefficients
+const sample_type_t fir_coeffs[NUM_TAPS] = {
+		-0.016430137208605464, 0.02688807608569583, 0.010738059913851562, -0.1207334193264336,
+		0.2542364968407787, 0.6846966843741256, 0.2542364968407787,
+		-0.1207334193264336, 0.010738059913851562, 0.02688807608569583, -0.016430137208605464
+};
+static arm_fir_instance_f32 fir_instance;
 
 
 void start_ecg_acqisition(void){
@@ -32,6 +50,9 @@ void start_ecg_acqisition(void){
 	//Set up double buffer
 	sample_ptr = sample_buffer1;
 	sample_end = sample_buffer1 + ADC_SAMPLE_COUNT;
+
+	// Init fir filter
+	arm_fir_init_f32(&fir_instance, NUM_TAPS, fir_coeffs, fir_state, BLOCK_SIZE);
 
 	// bring ECG Shutdown pin High
 	HAL_GPIO_WritePin(ECG_SDN_GPIO_Port, ECG_SDN_Pin, GPIO_PIN_SET);
@@ -57,7 +78,7 @@ void ecg_tim_callback(void){
 
 void ecg_adc_callback(uint32_t value){
 	// This is safe, value can only go up to 4096 which fits in a uint16_t
-	uint16_t sample = (uint16_t) value;
+	sample_type_t sample = (float) value * (1/4096.0f);
 	*sample_ptr++ = sample;
 
 	// Have we reached the end of the buffer?
@@ -75,5 +96,9 @@ void ecg_adc_callback(uint32_t value){
 		// May need a compiler memory barrier here.
 		buffer_finished = 1;
 	}
+}
+
+void run_ecg_filter(void){
+	arm_fir_f32(&fir_instance, buffer_ptr, filter_output, BLOCK_SIZE);
 }
 
--- wsp_src/Core/Src/main.c
+++ wsp_src/Core/Src/main.c
@@ -303,7 +303,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_DOWN;
-  htim2.Init.Period = 64000000;
+  htim2.Init.Period = 512000;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
